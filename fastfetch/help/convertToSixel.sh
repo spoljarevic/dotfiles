8d6ba7446f08c17b65dcdb8356404681330688e4
Luca Matteo Spoljarevic <lms@spoljarevic.sh>
2025-12-30 18:35:01 +0100

    Created a script that converts the image to sixel using chafa and displays fastfetch with the new image

---
+++ fastfetch/help/convertToSixel.sh
@@ -0,0 +1,21 @@
+# Go to assets folder where the image is
+cd /.config/fastfetch/assets
+
+# Gather information about the file name
+echo "What's the Name of the File (Without file type)?"
+read FILE_NAME
+
+# Gather information about file type
+echo "What's the File Type?"
+read FILE_TYPE
+
+# Gather information about the size
+echo "What's the Size (Width only)?"
+read FILE_WIDTH
+FILE_HEIGHT=$(echo "$FILE_WIDTH / 2" | bc -l | awk '{printf "%d", $1}')
+
+# Convert the image to sixel
+chafa -s "$FILE_WIDTH"x"$FILE_WIDTH" $FILE_NAME.$FILE_TYPE > $FILE_NAME.sixel
+
+# Display fastfetch with the image
+fastfetch --raw $FILE_NAME.sixel --logo-width $FILE_SIZE --logo-height $FILE_HEIGHT --logo-padding-top 2 
